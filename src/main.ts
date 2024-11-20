415df0e4c42e44dbad6c0de9967a86df42a8590e
ValentinHer <valentinhernandezhernandez18@gmail.com>
2024-11-19 18:13:10 -0600

    update main

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  app.enableCors({origin: "http://localhost:3000"});
+  app.enableCors({origin: "*"});
   await app.listen(process.env.PORT || 3000);
 }
 
