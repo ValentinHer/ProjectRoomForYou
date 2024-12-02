b99f4109f17568352caa39b3ff6f8867520be4e1
ValentinHer <valentinhernandezhernandez18@gmail.com>
2024-12-01 20:21:11 -0600

    [fix] changes in fileService dto

--- src/file/dto/create-file.dto.ts
+++ src/file/dto/create-file.dto.ts
@@ -6,5 +6,5 @@ export class CreateFileDto {
     @ApiProperty({example: '60e92b59-6a6d-4f8b-baca-c9fde9d1313a', description: 'Property Id'})
     @IsString()
     @IsNotEmpty()
-    propertyid: string;
+    propertyId: string;
 }
